08e7aa5c369b0186ff8c786bc18260f21e9b0f40
Oleksandr Shevchenko <shevchol69@gmail.com>
2025-09-22 01:58:32 +0300

    first commit

---
+++ tsk1_main.py
@@ -0,0 +1,13 @@
+class Editor:
+
+    def __init__(self, key):
+        self.key = key
+
+    def view_document(self):
+        pass
+
+    def edit_document(self):
+        pass
+
+    # class ProEditor:
+    #     def view_document(self):
