d12d71afb9bd39208a2558c829708569c1f621d5
Oleksandr Shevchenko <shevchol69@gmail.com>
2025-09-22 14:51:18 +0300

    tsk1 done

--- tsk1_main.py
+++ tsk1_main.py
@@ -1,13 +1,24 @@
 class Editor:
-
-    def __init__(self, key):
-        self.key = key
-
+    def __init__(self, license_key: str):
+        self.license_key = license_key
     def view_document(self):
-        pass
-
+        print("Перегдяд дозволений.")
     def edit_document(self):
-        pass
+        print("Редагування дозволено.")
 
-    # class ProEditor:
-    #     def view_document(self):
+class ProEditor(Editor):
+    def edit_document(self):
+        print("Редагування не дозволено.")
+
+license_key = "1111"
+while True:
+    key = input("Введіть ключ, або 'stop' для виходу: ")
+    if key == "stop":
+        break
+    elif key == license_key:
+        key_found = Editor(license_key)
+    else:
+        key_found = ProEditor(license_key)
+        
+    key_found.view_document()
+    key_found.edit_document()
